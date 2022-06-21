6eb764f4f67002dd8f086b09d2c81ac56afe2c40
zacharygawthorp <107508857+zacharygawthorp@users.noreply.github.com>
2022-06-21 11:24:49 -0500

    Add files via upload

---
+++ letConstExercise.js
@@ -0,0 +1,18 @@
+//** What is the difference between var and let? */
+
+//var defines a variable globally, or locally to an entire function regardless of block scope. It can also be redeclared.
+//let allows you to declare a variable, however it is limited to the block, statement, or expression in which it is being used.
+
+//** What is the difference between var and const */
+
+//var defines a variable globally, or locally to an entire function regardless of block scope. It can also be redeclared.
+//const variable is block scope. It cannot be re-declared into the scope and cannot be declared without initialization. 
+
+//** What is the difference between let and const */
+
+//let allows you to declare a variable, it can be updated, but cannot be re-declared into the scope. 
+//const declares a variable, however cannot be updated or re-declared into the scope. 
+
+//** What is hoisting */
+
+//variables are lifted to the top of the scoped they are declared in. 
